b12b1d0cc9de6901b8b37c6d7e3658d7d934b398
000 <000@192.168.12.74>
2019-08-27 15:30:55 +0800

    基础类库

---
+++ sp02-itemservice/src/main/java/com/tedu/sp02/item/service/ItemServiceImpl.java
@@ -0,0 +1,36 @@
+package com.tedu.sp02.item.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tedu.sp01.pojo.Item;
+import com.tedu.sp01.service.ItemService;
+
+import lombok.extern.slf4j.Slf4j;
+@Service
+@Slf4j
+public class ItemServiceImpl implements ItemService {
+
+	@Override
+	public List<Item> getItems(String OrderId) {
+		ArrayList<Item> list = new ArrayList<>();
+		list.add(new Item(1,"aaa",5));
+		list.add(new Item(2,"bbb",1));
+		list.add(new Item(3,"ccc",2));
+		list.add(new Item(4,"ddd",3));
+		list.add(new Item(5,"eee",4));
+		return list;
+	}
+	@Override
+	public void decreaseNumbers(List<Item> list) {
+		if (log.isInfoEnabled()) {
+			for (Item item : list) {
+				log.info("减少库存-"+item);
+			}
+		}
+		
+	}
+
+}
